2c0a604aeca50db9c730e5f8744fc5357f9c207a
jonahd345 <info.minejona@gmail.com>
2024-12-26 20:29:27 +0100

    edit: refactor EconomyTopPlayer

--- src/main/java/de/jonahd345/extendedeconomy/model/EconomyTopPlayer.java
+++ src/main/java/de/jonahd345/extendedeconomy/model/EconomyTopPlayer.java
@@ -1,7 +1,10 @@
 package de.jonahd345.extendedeconomy.model;
 
+import lombok.Getter;
+
 import java.util.UUID;
 
+@Getter
 public class EconomyTopPlayer {
     private UUID uuid;
 
@@ -13,17 +16,5 @@ public class EconomyTopPlayer {
         this.uuid = uuid;
         this.name = name;
         this.coins = coins;
-    }
-
-    public UUID getUuid() {
-        return this.uuid;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public double getCoins() {
-        return this.coins;
     }
 }
